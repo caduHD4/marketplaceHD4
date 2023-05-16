04bcafbeb05df4bb5d6989bd6a2a134731805668
caduHD4 <cadumarianogarciapereira@gmail.com>
2023-05-16 20:02:25 -0300

    arquivos spring

---
+++ loginha/src/main/java/com/example/loginha/LoginhaApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginhaApplication.class, args);
+	}
+
+}
---
+++ loginha/src/test/java/com/example/loginha/LoginhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
